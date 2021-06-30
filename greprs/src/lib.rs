4b3ed405f91356f3bf8d3c99e01efe66be2743fe
DESKTOP-E8S5FMF\백종환 <bsrk1013@naver.com>
2021-06-30 14:29:49 +0900

    공부

--- greprs/src/lib.rs
+++ greprs/src/lib.rs
@@ -1,21 +1,102 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
 pub struct Config {
-    query: String,
-    filename: String,
+    pub query: String,
+    pub filename: String,
+    pub is_insensitive: bool,
 }
 
 impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() != 3 {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
             return Err("not enough arguments");
         }
 
         let query = args[1].clone();
         let filename = args[2].clone();
+        let is_insensitive = env::var("IS_INSENSITIVE").is_err();
 
         if !filename.contains(".txt") {
             return Err("illegal filename");
         }
 
-        Ok(Config { query, filename })
+        Ok(Config {
+            query,
+            filename,
+            is_insensitive,
+        })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), std::io::Error> {
+    let mut contents = String::new();
+    let mut file = File::open(config.filename)?;
+    file.read_to_string(&mut contents)?;
+
+    let result = if config.is_insensitive {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+    for line in result {
+        println!("{}", line);
+    }
+
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = vec![];
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+    result
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = vec![];
+    let query = query.to_lowercase();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            result.push(line);
+        }
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
--- greprs/src/main.rs
+++ greprs/src/main.rs
@@ -1,53 +1,17 @@
+extern crate greprs;
+
 use std::env;
-use std::fs::File;
-use std::io;
-use std::io::prelude::*;
 use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("{}", err);
+    let config = greprs::Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("{}", err);
         process::exit(1);
     });
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
 
-    let content = get_file_content(&config.filename).expect("read content failed...");
-    println!(
-        "content
----------------------------------------------
-{}
----------------------------------------------",
-        content
-    );
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() != 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        if !filename.contains(".txt") {
-            return Err("illegal filename");
-        }
-
-        Ok(Config { query, filename })
-    }
-}
-
-fn get_file_content(filename: &str) -> Result<String, io::Error> {
-    let mut content = String::new();
-    File::open(filename)?.read_to_string(&mut content)?;
-
-    Ok(content.clone())
+    greprs::run(config).unwrap_or_else(|err| {
+        eprintln!("{}", err);
+        process::exit(1);
+    });
 }
